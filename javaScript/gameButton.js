a75c57e4aefec3efb9a43a150d2aa7c9ad4a1604
Damcek <damm.marko@gmail.com>
2022-08-14 14:29:20 +0200

    just making useless js script work again

--- javaScript/gameButton.js
+++ javaScript/gameButton.js
@@ -1,51 +1,51 @@
-// // let gameButton = document.querySelector('.header__nav--game-room');
+// // // let gameButton = document.querySelector('.header__nav--game-room');
 
-// // console.log(gameButton);
+// // // console.log(gameButton);
 
-// // let style = gameButton['style'];
+// // // let style = gameButton['style'];
 
-// // console.log(style);
+// // // console.log(style);
 
-// // let bg_img = style['background-image'];
+// // // let bg_img = style['background-image'];
 
-// // console.log(bg_img);
+// // // console.log(bg_img);
 
-// // let color;
-// // function changeColor () {
-// //   return color = 'radial-gradient(rgb(0, 0, 255) 25%, transparent 70%)';
-// // }
-
-// // changeColor();
-
-// // console.log(color);
-
-
-
-
-
-// // setTimeout (colorTiming, 10) 
-// // function colorTiming() {
-
+// // // let color;
+// // // function changeColor () {
+// // //   return color = 'radial-gradient(rgb(0, 0, 255) 25%, transparent 70%)';
 // // // }
-// // // let i = 1;
-// // // while (i < 10) {
-// // // console.log('hi');
-// // // i++
-// // // };
-// document.getElementById("hi").style.backgroundImage = `radial-gradient(rgb(0, 0, 255) 25%, transparent 70%)`;
+
+// // // changeColor();
+
+// // // console.log(color);
+
+
+
+
+
+// // // setTimeout (colorTiming, 10) 
+// // // function colorTiming() {
+
+// // // // }
+// // // // let i = 1;
+// // // // while (i < 10) {
+// // // // console.log('hi');
+// // // // i++
+// // // // };
+// // document.getElementById("hi").style.backgroundImage = `radial-gradient(rgb(0, 0, 255) 25%, transparent 70%)`;
 
 // const sleep = (milliseconds) => {
 //   return new Promise(resolve => setTimeout(resolve, milliseconds))
 // }
 
 // const changeColor = async () => {
-//   for (let i = 0; i < 1; i++) {
+//   for (let i = 0; i < 1; i) {
 
 //     for(let g = 0; g < 256; g++) {
 //       await sleep(1)
 //       let r = 0;
 //       let b = 255 - g;
-//       document.getElementById("hi").style.backgroundImage.replace(`${255-g}`, `${b}`);
+//       document.getElementById("hi").style.backgroundImage = `radial-gradient(rgb(${r}, ${g}, ${b}) 25%, transparent 70%)` ;
 //     }
 //     for(let r = 0; r < 256; r++) {
 //       await sleep(1)
@@ -64,4 +64,4 @@
 // }
 
 
-// document.getElementById("hi").style.backgroundImage.replace(`255`, `100`);
+// changeColor()
